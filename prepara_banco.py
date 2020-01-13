72360c1257798ae4c624b32f22fab4a87ff7ad40
Maiconfsantos <maiconfsanto@gmail.com>
2020-01-13 16:07:40 -0300

    Commit Inicial

---
+++ prepara_banco.py
@@ -0,0 +1,61 @@
+import MySQLdb
+print('Conectando...')
+conn = MySQLdb.connect(user='u130015149_admin_estudos', passwd='A130099821', host='185.201.10.94', port=3306)
+
+# Descomente se quiser desfazer o banco...
+# conn.cursor().execute("DROP DATABASE `u130015149_estudos`;")
+# conn.commit()
+
+criar_tabelas = '''SET NAMES utf8;
+    USE `u130015149_estudos`;
+    CREATE TABLE `jogo` (
+      `id` int(11) NOT NULL AUTO_INCREMENT,
+      `nome` varchar(50) COLLATE utf8_bin NOT NULL,
+      `categoria` varchar(40) COLLATE utf8_bin NOT NULL,
+      `console` varchar(20) NOT NULL,
+      PRIMARY KEY (`id`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+    CREATE TABLE `usuario` (
+      `id` varchar(8) COLLATE utf8_bin NOT NULL,
+      `nome` varchar(20) COLLATE utf8_bin NOT NULL,
+      `senha` varchar(8) COLLATE utf8_bin NOT NULL,
+      PRIMARY KEY (`id`)
+    ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;'''
+
+conn.cursor().execute(criar_tabelas)
+
+# inserindo usuarios
+cursor = conn.cursor()
+cursor.executemany(
+      'INSERT INTO u130015149_estudos.usuario (id, nome, senha) VALUES (%s, %s, %s)',
+      [
+            ('luan', 'Luan Marques', 'flask'),
+            ('nico', 'Nico', '7a1'),
+            ('danilo', 'Danilo', 'vegas')
+      ])
+
+cursor.execute('select * from u130015149_estudos.usuario')
+print(' -------------  Usuários:  -------------')
+for user in cursor.fetchall():
+    print(user[1])
+
+# inserindo jogos
+cursor.executemany(
+      'INSERT INTO u130015149_estudos.jogo (nome, categoria, console) VALUES (%s, %s, %s)',
+      [
+            ('God of War 4', 'Ação', 'PS4'),
+            ('NBA 2k18', 'Esporte', 'Xbox One'),
+            ('Rayman Legends', 'Indie', 'PS4'),
+            ('Super Mario RPG', 'RPG', 'SNES'),
+            ('Super Mario Kart', 'Corrida', 'SNES'),
+            ('Fire Emblem Echoes', 'Estratégia', '3DS'),
+      ])
+
+cursor.execute('select * from u130015149_estudos.jogo')
+print(' -------------  Jogos:  -------------')
+for jogo in cursor.fetchall():
+    print(jogo[1])
+
+# commitando senão nada tem efeito
+conn.commit()
+cursor.close()
