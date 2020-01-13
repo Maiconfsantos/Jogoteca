2276b0a96cfca230c695fb1186692feb9186ce9e
Maiconfsantos <maiconfsanto@gmail.com>
2020-01-13 16:51:58 -0300

    Divisão de arquivos

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+SECRET_KEY = 'alura'
+MYSQL_HOST = '185.201.10.94'
+MYSQL_USER = 'u130015149_admin_estudos'
+MYSQL_PASSWORD = 'A130099821'
+MYSQL_DB = 'u130015149_estudos'
+MYSQL_PORT = 3306
+UPLOAD_PATH = os.path.dirname(os.path.abspath(__file__)) + '/uploads'
